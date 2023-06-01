47532f6d367426d6f6b196063f0d8d39f500e09c
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-06-01 15:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+// Promise 1
+window.promises.push(
+  new Promise((resolve) => setTimeout(() => resolve("Promise 1"), Math.random() * 5000 + 1000))
+);
+
+// Promise 2
+window.promises.push(
+  new Promise((resolve) => setTimeout(() => resolve("Promise 2"), Math.random() * 5000 + 1000))
+);
+
+// Promise 3
+window.promises.push(
+  new Promise((resolve) => setTimeout(() => resolve("Promise 3"), Math.random() * 5000 + 1000))
+);
+
+// Promise 4
+window.promises.push(
+  new Promise((resolve) => setTimeout(() => resolve("Promise 4"), Math.random() * 5000 + 1000))
+);
+
+// Promise 5
+window.promises.push(
+  new Promise((resolve) => setTimeout(() => resolve("Promise 5"), Math.random() * 5000 + 1000))
+);
